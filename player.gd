561fbbfa796ba46c22e0edca57cb695bec42f48d
BenMyhillJones <161653222+BenMyhillJones@users.noreply.github.com>
2025-12-01 13:03:10 -0800

    Basic Movement Shell

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var speed := 400.0
+@export var jump_force := -600.0
+@export var gravity := 1400.0
+
+@export var max_jumps := 10000   # 1 = normal jump, 2 = double jump
+var jumps_left := 0
+
+func _ready():
+	jumps_left = max_jumps
+
+func _physics_process(delta):
+	# Apply gravity if not on floor
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		velocity.x = velocity.x + 1
+	else:
+		# Reset jumps when touching the ground
+		jumps_left = max_jumps
+
+	# Horizontal movement
+	var direction = Input.get_axis("MoveLeft", "MoveRight")
+	velocity.x = direction * speed
+
+	# Jump (allows double jump)
+	if Input.is_action_just_pressed("Jump") and jumps_left > 0:
+		velocity.y = jump_force
+		jumps_left -= 1
+
+	move_and_slide()
