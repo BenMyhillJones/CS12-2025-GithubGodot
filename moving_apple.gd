e743853a27167b2302377618daf76cd372bdd2a5
Rovesib <1468172@sd61learn.ca>
2025-12-03 13:28:10 -0800

    moving apple

--- moving_apple.gd
+++ moving_apple.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-
+var movespeed := 200.0
 
 func _ready():
-	velocity = Vector2(200, 0)
+	velocity = Vector2(movespeed, 0)
 
 func _physics_process(delta: float) -> void:
+	velocity.x = movespeed
 	move_and_slide()
-	
+
 	if is_on_wall():
-		velocity = -velocity
+		movespeed = -movespeed
+
+	if movespeed > 0:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
